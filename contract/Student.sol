862f9cc2ac0f9adec836d2969161e58e6a915a8c
tstctzyh <nik7880@gmail.com>
2020-04-04 17:58:26 +0700

    update add courese and register

--- contract/Student.sol
+++ contract/Student.sol
@@ -4,38 +4,71 @@ pragma experimental ABIEncoderV2;
 contract Student {
 //   StudentData[] Students;
 
-  struct StudentData {
-      address student_address;
-      string  student_id;
-      string  name;
-      string  lastname;
-    //   bytes32  birth_date;
-    //   bytes32  email;
-    //   bytes32  home_address;
-    //   bytes32  level;
-    //   bytes32  school;
-    //   bytes32  major;
-    //   bytes32  admid_date;
-    //   bytes32  status;
-  }
+    struct StudentData {
+        address student_address;
+        string  student_id;
+        string  name;
+        string  lastname;
+    }
 
+    struct CourseData {
+        string  course_id;
+        string  course_name;
+        uint    credit;
+    }
 
+    struct RegisterData{
+        uint8   register_id;
+        uint    year;
+        uint    semester;
+        address student_address;
+        string  student_id;
+        string  name;
+        string  lastname;
+        string  course_id;
+        string  course_name;
+        uint     course_credit;
+        uint     score;
+        string  grade;
+        uint    block_number;
+        uint    block_time;
+    }
 
-  mapping (address=>StudentData) private Students;
-  address[] private addressStudents;
+    mapping (address=>StudentData) private Students;
+    address[] private addressStudents;
 
-  constructor () public{
+    mapping (string=>CourseData) private Courses;
+    string[] private courseIdAll;
 
-  }
+    mapping (uint8=>RegisterData) private Registers;
+    uint8[] private registerIdAll;
 
-  function testSlice() public view returns (address){
-    //   address cur_address = msg.sender;
-      address a = address(msg.sender);
+    constructor () public{
 
-      return a;
+    }
 
-  }
+    function random() private view returns (uint8) {
+       return uint8(uint256(keccak256(abi.encodePacked((block.timestamp)))))%251;
+   }
 
+   function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
 
     function getStudents() public view returns (StudentData[] memory){
 
@@ -60,6 +93,7 @@ contract Student {
             bytes memory std_check = bytes(total_student[i].student_id);
             if(keccak256(abi.encodePacked((std_check))) == keccak256(abi.encodePacked((std_where)))){
                 student_search = total_student[i];
+                return student_search;
             }
         }
 
@@ -68,7 +102,7 @@ contract Student {
     }
 
 
-    function setStudent (string memory _name,string memory _lastame ) public checkDuplicate{
+    function addStudent (string memory _name,string memory _lastame ) public checkDuplicate{
         addressStudents.push(msg.sender);
         StudentData storage o = Students[msg.sender];
         o.student_address = msg.sender;
@@ -98,23 +132,107 @@ contract Student {
 
     }
 
-   function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
+    function checkDuplicateCourse(string memory _course_id) internal returns (bool){
+        bytes memory course_where = bytes(_course_id);
+        CourseData[] memory total_course = new CourseData[](courseIdAll.length);
+        for (uint i=0; i<courseIdAll.length; i++) {
+            bytes memory course_check = bytes(total_course[i].course_id);
+            if(keccak256(abi.encodePacked((course_check))) == keccak256(abi.encodePacked((course_where)))){
+                return false;
+            }
         }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len - 1;
-        while (_i != 0) {
-            bstr[k--] = byte(uint8(48 + _i % 10));
-            _i /= 10;
-        }
-        return string(bstr);
+
+        return true;
     }
+
+    function addCourse (string memory _course_id,string memory _course_name,uint _credit ) public {
+        bool check_dup = checkDuplicateCourse(_course_id);
+        require(check_dup);
+
+        courseIdAll.push(_course_id);
+        CourseData storage o = Courses[_course_id];
+        o.course_id = _course_id;
+        o.course_name = _course_name;
+        o.credit = _credit;
+
+    }
+
+     function getCourses() public view returns (CourseData[] memory){
+
+        CourseData[] memory total_course = new CourseData[](courseIdAll.length);
+        for (uint i=0; i<courseIdAll.length; i++) {
+            total_course[i] = Courses[courseIdAll[i]];
+        }
+
+        return total_course;
+    }
+
+
+    function getCourseByCourseID(string memory coures_id) public view returns (CourseData memory){
+        bytes memory course_where = bytes(coures_id);
+        CourseData[] memory total_course = new CourseData[](courseIdAll.length);
+        CourseData memory course_search;
+        for (uint i=0; i<courseIdAll.length; i++) {
+            total_course[i] = Courses[courseIdAll[i]];
+            bytes memory course_check = bytes(total_course[i].course_id);
+            if(keccak256(abi.encodePacked((course_check))) == keccak256(abi.encodePacked((course_where)))){
+                course_search = total_course[i];
+                return course_search;
+            }
+        }
+
+        return course_search;
+    }
+
+    function checkCourseRegister(uint _year,uint _semester,string memory _course_id)  internal returns (bool){
+        //address student
+        address student_address = msg.sender;
+        bytes memory course_where = bytes(_course_id);
+
+        RegisterData[] memory total_register = new RegisterData[](registerIdAll.length);
+        for (uint i=0; i<registerIdAll.length; i++) {
+            total_register[i] = Registers[registerIdAll[i]];
+            bytes memory course_check = bytes(_course_id);
+            if(
+                keccak256(abi.encodePacked((course_check))) == keccak256(abi.encodePacked((course_where))) &&
+                total_register[i].student_address == student_address &&
+                total_register[i].year == _year &&
+                total_register[i].semester == _semester
+            ){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    function courseRegister(uint _year,uint _semester,string memory _course_id) public {
+        StudentData memory myprofile = getMyProfile();
+        CourseData memory course_select= getCourseByCourseID(_course_id);
+
+        bool stat_register = checkCourseRegister(_year,_semester,_course_id);
+        if(stat_register==true){
+            require(false);
+        }
+
+        uint8 register_id = random();
+        registerIdAll.push(register_id);
+        RegisterData storage o = Registers[register_id];
+        o.year=_year;
+        o.semester=_semester;
+        o.student_address=msg.sender;
+        o.student_id=myprofile.student_id;
+        o.name=myprofile.name;
+        o.lastname=myprofile.lastname;
+        o.course_id = course_select.course_id;
+        o.course_name = course_select.course_name;
+        o.course_credit = course_select.credit;
+        o.score=0;
+        o.grade="-";
+        o.block_number=block.number;
+        o.block_time=block.timestamp;
+    }
+
 
 }
